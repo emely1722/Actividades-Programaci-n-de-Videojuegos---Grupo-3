223b86a1cc13cb17485cae6afffb908d9952e65b
Omargenao25 <omargenao78@gmail.com>
2026-06-04 00:39:46 -0400

    creacion controlador del jugador

---
+++ Actividad1-Grupo3/Assets/PlayerController.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 5f;
+    public float jumpForce = 7f;
+    private Rigidbody2D rb;
+    private bool isGrounded;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
