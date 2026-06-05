3a167b40a146811ae59139c8a5e092f7f0a86158
Omargenao25 <omargenao78@gmail.com>
2026-06-04 21:03:52 -0400

    avances en el controller

--- Actividad1-Grupo3/Assets/PlayerController.cs
+++ Actividad1-Grupo3/Assets/PlayerController.cs
@@ -6,15 +6,46 @@ public class PlayerController : MonoBehaviour
     public float jumpForce = 7f;
     private Rigidbody2D rb;
     private bool isGrounded;
+    private float moveInput;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // Capturamos el movimiento
+        moveInput = Input.GetAxisRaw("Horizontal");
+
+        // Salto
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+        }
     }
+
+    void FixedUpdate()
+    {
+        // Movimiento por físicas
+        rb.linearVelocity = new Vector2(moveInput * speed, rb.linearVelocity.y);
+    }
+
+    
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    } 
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = false;
+        }
+    } 
 }
