92ad5cd9a7faac327a2b64e0d96fc100fe53ae23
Omargenao25 <omargenao78@gmail.com>
2026-06-22 15:30:24 -0400

    cambios en controller

--- Actividad1-Grupo3/Assets/PlayerController.cs
+++ Actividad1-Grupo3/Assets/PlayerController.cs
@@ -4,42 +4,101 @@ public class PlayerController : MonoBehaviour
 {
     public float speed = 5f;
     public float jumpForce = 7f;
+    public float fallMultiplier = 2.5f;
+    public float lowJumpMultiplier = 2f;
+    public float maxFallSpeed = -15f;
+
+    public float coyoteTime = 0.2f;
+    private float coyoteTimeCounter;
+
+    public float jumpBufferTime = 0.2f;
+    private float jumpBufferCounter;
+
     private Rigidbody2D rb;
     private bool isGrounded;
     private float moveInput;
-
+    private SpriteRenderer spriteRenderer;
+    private Animator animator;    
+     
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
     }
 
     void Update()
     {
-        // Capturamos el movimiento
         moveInput = Input.GetAxisRaw("Horizontal");
 
-        // Salto
-        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        animator.SetFloat("Velocidad horizontal", Mathf.Abs(moveInput));
+
+        if (moveInput > 0)
+        {
+            spriteRenderer.flipX = false;
+        }
+        else if (moveInput < 0)
+        {
+            spriteRenderer.flipX = true;
+        }
+
+        if (isGrounded)
+        {
+            coyoteTimeCounter = coyoteTime;
+        }
+        else
+        {
+            coyoteTimeCounter -= Time.deltaTime;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            jumpBufferCounter = jumpBufferTime;
+        }
+        else
+        {
+            jumpBufferCounter -= Time.deltaTime;
+        }
+
+        if (jumpBufferCounter > 0f && coyoteTimeCounter > 0f)
         {
             rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+
+            animator.SetBool("IsJumping", true);
+
+
+            jumpBufferCounter = 0f;
+            coyoteTimeCounter = 0f;
+        }
+
+        if (rb.linearVelocity.y < 0)
+        {
+            rb.linearVelocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
+        }
+        else if (rb.linearVelocity.y > 0 && !Input.GetKey(KeyCode.Space))
+        {
+            rb.linearVelocity += Vector2.up * Physics2D.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
+        }
+
+        if (rb.linearVelocity.y < maxFallSpeed)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, maxFallSpeed);
         }
     }
 
     void FixedUpdate()
     {
-        // Movimiento por físicas
         rb.linearVelocity = new Vector2(moveInput * speed, rb.linearVelocity.y);
     }
-
-    
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Ground"))
         {
             isGrounded = true;
+            animator.SetBool("IsJumping", false);
         }
-    } 
+    }
 
     private void OnCollisionExit2D(Collision2D collision)
     {
@@ -47,5 +106,5 @@ public class PlayerController : MonoBehaviour
         {
             isGrounded = false;
         }
-    } 
+    }
 }
