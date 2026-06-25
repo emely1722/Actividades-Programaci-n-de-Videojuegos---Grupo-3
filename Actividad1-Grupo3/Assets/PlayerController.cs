4a7af727322fc34b04365c6765ee11220cf0d453
emely1722 <66326292+emely1722@users.noreply.github.com>
2026-06-25 19:54:52 -0400

    activando animaciones de una fruta

--- Actividad1-Grupo3/Assets/PlayerController.cs
+++ Actividad1-Grupo3/Assets/PlayerController.cs
@@ -106,4 +106,14 @@ public class PlayerController : MonoBehaviour
             isGrounded = false;
         }
     }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            GetComponent<SpriteRenderer>().enabled = false;
+            gameObject.transform.GetChild(0).gameObject.SetActive(true);
+            Destroy(gameObject, 0.5f);
+        }
+    }
 }
